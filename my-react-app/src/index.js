718a2c63e7c4191e16a87b79a6d02bec010b4de3
UddinKashem <30503392+UddinKashem@users.noreply.github.com>
2025-05-13 13:45:06 -0400

    Create Router and Native App

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -15,3 +15,14 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+//
+//import { createRoot } from 'react-dom/client';
+//
+//function NavigationBar() {
+//  // TODO: Actually implement a navigation bar
+//  return <h1>Hello from React!</h1>;
+//}
+//
+//const domNode = document.getElementById('navigation');
+//const root = createRoot(domNode);
+//root.render(<NavigationBar />);
